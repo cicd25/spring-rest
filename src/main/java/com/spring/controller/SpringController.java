8c6486d93eb0ab91873a3924553bdb37310c5351
root <root@wls.server.com>
2020-12-06 20:37:43 +0530

    message updated

--- src/main/java/com/spring/controller/SpringController.java
+++ src/main/java/com/spring/controller/SpringController.java
@@ -11,12 +11,12 @@ public class SpringController implements Serializable {
 
     @GetMapping("/hello")
     public String showHelloMsg() {
-      return "Welcome to SpringBoot from the Ansible Playbook....";
+      return "Welcome to SpringBoot from the Jenkins + Ansible Playbook....";
     }
 
     @GetMapping("/")	
     public String displayMsg() {
-      return "welcome to github-webhook - docker - ansible "; 
+      return "welcome to github-webhook - Jenkins, Ansible and Docker "; 
     }
 
 }
